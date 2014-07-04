a2f0c992d601975a37f3f27386a71111aba14ae6
marpoolchen <marpool119@163.com>
2014-07-04 14:53:20 +0800

    day 1
    change main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int h	=	31;
+de ma xi ya 
+int mMap = 12;
+int a = 18;
+char cha;
+
+int b = 14;
+int m = 33;
+int main()
+{
+
+	return 0;
+}
+int aaa();
+int n;
+char* name = "dominic";
+sdsd
+wo lai le
